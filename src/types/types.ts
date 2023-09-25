035091b5bb1b5a0c027ba669cfd0087c0a44a7eb
Adam MacDonald <am1macdonald@gmail.com>
2023-09-25 07:55:01 -0400

    Define Command interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,9 @@ export type Dimensions = { height: number; width: number };
 export type Coordinate = { x: number; y: number };
 
 export type Edge = [Vertex, Vertex];
+
+export type Command = (execute: (...args: unknown[]) => unknown, value: unknown) => void;
+
+export interface Controllable {
+  execute: (command: Command) => void;
+}
